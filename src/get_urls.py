b6e38b3a6b979b25dad684d8a6042b0f57edda6b
gaBUBUNts <89221310+gaBUBUNts@users.noreply.github.com>
2022-05-14 13:42:46 +0300

    UPD get_urls.py - добавлена функция для получения ссылок закладок браузера

--- src/get_urls.py
+++ src/get_urls.py
@@ -1,7 +1,8 @@
+import os
 import sqlite3
 
 
-def get_urls(user, profile="Default") -> list[tuple]:
+def get_urls_from_history(user, profile="Default") -> list[tuple]:
     """
     Вернуть из истории браузера список ссылок и названия сайтов
 
@@ -9,12 +10,33 @@ def get_urls(user, profile="Default") -> list[tuple]:
     :param profile: имя профиля браузера, если профиля нет - Default
     :return: list[tuple], где каждый tuple это (url, title)
     """
-    history = sqlite3.connect(f"C:\\Users\\{user}\\AppData\\Local\\"
-                              f"Google\\Chrome\\User Data\\{profile}\\History")
+    history = sqlite3.connect(os.path.join(r"C:\Users",
+                                           user,
+                                           r"AppData\Local\Google\Chrome\User Data",
+                                           profile,
+                                           "History"))
     cur = history.cursor()
     cur.execute("SELECT  url, title FROM urls")
     return cur.fetchall()
 
 
-if __name__ == "__main__":
-    print(get_urls("proil", "Profile 1"))
+def get_url_from_bookmarks(user, profile="Default") -> list[str]:
+    """
+    Вернуть из закладок браузера список ссылок.
+
+    :param user: имя пользователя пк
+    :param profile: имя профиля браузера, если профиля нет - Default
+    :return: list[str], где каждый элемент это ссылка
+    """
+    with open(os.path.join(r"C:\Users",
+                           user,
+                           r"AppData\Local\Google\Chrome\User Data",
+                           profile,
+                           "Bookmarks"),
+              "r", encoding="utf-8") as bookmarks:
+        urls = []
+        for line in bookmarks:
+            index = line.find('"url": ')
+            if index != -1:
+                urls.append(line[index + 8:-2])
+    return urls
