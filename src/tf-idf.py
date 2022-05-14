e2a67d3e635a546f81abd7848b336e9b60aed77b
Timofey Bolotov <timbolotov3@gmail.com>
2022-05-14 12:07:07 +0300

    новый способ сравнения

--- src/tf-idf.py
+++ src/tf-idf.py
@@ -1,59 +1,14 @@
-import collections
-import math
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def splittolist(page):
-	f = open(page, encoding='utf-8')
-	splitted = []
-	for line in f:
-		splitted.append(line.split())
-	f.close()
-	return splitted
+with open('file1', 'r', encoding = 'utf-8') as F:
+    inception = F.read()
 
+with open('file2', 'r', encoding = 'utf-8') as F:
+    shutter_island = F.read()
 
-def flat(list_):
-	flat = []
-	for sublist in list_:
-		for elem in sublist:
-			flat.append(elem)
-	return flat
-
-
-def computetf(text):
-	tf_text = collections.Counter(text)
-	for i in tf_text:
-		tf_text[i] = tf_text[i] / float(len(text))
-	return tf_text
-
-
-def computeidf(word, corpus):
-	return math.log10(len(corpus) / sum([1.0 for i in corpus if word in i]))
-
-
-def computetf_idf(list_):
-	'''Принимает список из плоских списков'''
-	documents_list = []
-	for text in list_:
-		tf_idf_dictionary = {}
-		computed_tf = computetf(text)
-		for word in computed_tf:
-			tf_idf_dictionary[word] = computed_tf[word] * computeidf(word, list_)
-		documents_list.append(tf_idf_dictionary)
-	return (documents_list)
-
-
-if __name__ == '__main__':
-	# print(splittolist('page1.txt'))
-	wordlist1 = splittolist('test1.txt')
-	flat_wordlist1 = flat(wordlist1)
-	wordlist2 = splittolist('test2.txt')
-	flat_wordlist2 = flat(wordlist2)
-	wordlist3 = splittolist('test3.txt')
-	flat_wordlist3 = flat(wordlist3)
-	
-	mainlist = [flat_wordlist1, flat_wordlist2, flat_wordlist3]
-	# print(flat_wordlist3)
-	
-	# print(computetf(flat_wordlist1))
-	# print(computetf(flat_wordlist2))
-	print(sorted(computetf_idf(mainlist)[0]))
+tfidf = TfidfVectorizer()
+vecs = tfidf.fit_transform([inception, shutter_island])
+corr_matrix = ((vecs * vecs.T).A)
+similarity = corr_matrix[0,1]
+print(similarity)
