a691b030b0a22239ef554bb3c3c89483b29fa6e1
Timofey Bolotov <timbolotov3@gmail.com>
2022-05-14 12:26:31 +0300

    fix

--- src/tf-idf.py
+++ src/tf-idf.py
@@ -2,13 +2,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 with open('file1', 'r', encoding = 'utf-8') as F:
-    inception = F.read()
+    file1 = F.read()
 
 with open('file2', 'r', encoding = 'utf-8') as F:
-    shutter_island = F.read()
+    file2 = F.read()
 
 tfidf = TfidfVectorizer()
-vecs = tfidf.fit_transform([inception, shutter_island])
+vecs = tfidf.fit_transform([file1, file2])
 corr_matrix = ((vecs * vecs.T).A)
 similarity = corr_matrix[0,1]
 print(similarity)
