6b95ae6e92e2114ee93e5d679cce890976fc2d2b
Timofey Bolotov <timbolotov3@gmail.com>
2022-05-14 16:41:53 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 from get_urls import *
-from new_parser import *
+from parser1 import *
 from tf_idf import *
 
-
-
-if __name__ == '__main__':
+def final():
+    answer = []
     f = get_urls_from_history('Professional', 'Chrome', 'Profile 1')
     #parser('https://python.org', 'both')
     standart = parser('https://python.org')
     for i in f:
         if comparison(standart, i[0]) >= 0.05:
-            print(i[0])
+            answer.append(i[0])
+            #print(i[0])
+    return answer
+
+if __name__ == '__main__':
+    final()
         #print(parser(i[0]))
 
     #print(get_urls_from_history('Professional', 'Chrome', 'Profile 1'))
